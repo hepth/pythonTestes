445ac5f5835982d21eb7d9cef6aa0065538b6659
Matheus Castro <mbcastro.ufmg@gmail.com>
2019-06-04 00:41:27 -0300

    Debut avec numpy

--- Python_inicio/numpy.py
+++ Python_inicio/numpy.py
@@ -1,3 +1,5 @@
 #Maintenant on va essayer de jouer avec le paquet numpy (je vais écrire en français pour améliorer mon vocabulaire.)
 
 import numpy as np
+echantillon = np.random.normal(3,2,500) #crée une echantillon de 500 observations avec distribuition normale, moyenne 3 et écart type 2
+
