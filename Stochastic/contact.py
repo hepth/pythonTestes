bfccda5851d3a92c57bdb448a0b3e32c1233c11a
Matheus Castro <mbcastro.ufmg@gmail.com>
2019-06-06 20:03:03 -0300

    Adicionando processo de contato

--- Stochastic/contact.py
+++ Stochastic/contact.py
@@ -1,7 +1,7 @@
 #Ce programme tentera de créer une simulation du processus de contact proposé par Harris (1974).
 
 import numpy as np
-#import seaborn as sb
+import seaborn as sb
 #import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -23,7 +23,32 @@ grade[25,25,0] = 1
 #    return retorno
 
 #lala = conversor(grade)
-
+infecta = 5
+cura = 1
+def evolucao(dados):
+    d1,d2,d3 = dados.shape
+    doencaNorte = np.zeros([d1,d2,500])
+    doencaLeste = doencaNorte.copy()
+    doencaOeste = doencaNorte.copy()
+    doencaSul = doencaNorte.copy()
+    curas = doencaNorte.copy()
+    for i in range(d1):
+        for j in range(d2):  
+            doencaNorte[i,j,:] = np.random.exponential(infecta*25,500)
+            doencaLeste[i,j,:] = np.random.exponential(infecta*25,500)
+            doencaOeste[i,j,:] = np.random.exponential(infecta*25,500)
+            doencaSul[i,j,:] = np.random.exponential(infecta*25,500)
+            curas[i,j,:] = np.random.exponential(cura*25,500)
+    for k in np.arange(1,d3,1):
+        for i in range(d1):
+            for j in range(d2):
+                if(dados[i,j,k]==1):
+                    
+                    
+        
+        
+    
+#np.random.exponential(1/5,5)
 
 
 fig = plt.figure()
@@ -39,6 +64,3 @@ def animacao(t):
 
 ani = animation.FuncAnimation(fig,animacao,interval=250)
 plt.show()
-
-
-
